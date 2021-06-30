145b8e4ef650f376e7cfc6b129933f7c3260b753
ramin-sh <33373883+ramin-sh@users.noreply.github.com>
2021-06-30 09:34:46 +0000

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.views import generic
+from .models import Post
+# from django.core.paginator  import Paginator
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    paginate_by = 4#new
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
