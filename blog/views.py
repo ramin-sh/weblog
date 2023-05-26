803173cf628a693fc475f0b13d1da97a4a25832a
ramin-sh <33373883+ramin-sh@users.noreply.github.com>
2023-05-26 13:44:25 +0330

    Update views.py
    اضافه کردن ویو برای هندل کردن فرم نظر دادن در وبلاگ

--- blog/views.py
+++ blog/views.py
@@ -15,3 +15,23 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+ # new ------------------------
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+            self.object = self.get_object()
+            form = CommentForm(request.POST)
+
+            if form.is_valid():
+                # Save the form data to the database
+                my_model = form.save(commit=False)
+                print(type(my_model))
+
+                my_model.blog_post = self.object  # Assign the foreign key
+                
+                my_model.save()
+
+                return render(request, self.template_name, self.get_context_data())  
