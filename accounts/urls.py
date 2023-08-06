5bfd3196351c8aad0d1ebd8ddcf5ec73e51ca63b
ramin-sh <33373883+ramin-sh@users.noreply.github.com>
2023-08-06 16:37:42 +0330

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('show', views.show_post, name='show'),
+    path('', views.loginView, name='loginView'),
+    path('add_post', views.add_post, name='add_post'),
     path('logout/',views.logoutView),
     path('faramooshi',views.faramooshi),
     path('setpass',views.set_pass),
