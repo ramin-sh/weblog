26c08771940d5421e636086bb96ac476cd651fa8
ramin shahverdy <33373883+ramin-sh@users.noreply.github.com>
2024-05-20 07:41:25 +0330

    Update - views.py

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,7 @@ from django.views import generic
 from .models import Post,Comment
 from django.shortcuts import render
 # from django.core.paginator  import Paginator
-
-
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     paginate_by = 4#new
